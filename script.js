44704f7420399141f286d8862746bd35a11f4f72
Rohit Panmand <panmandrohit7@gmail.com>
2024-01-31 14:44:52 +0000

    Added a javascript file to make the fan rotate using buttons

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 let a = document.getElementById("img");
 function myfunon() {
-  a.style.animationDuration = 3 + "s";
+  a.style.animationDuration = 2 + "s";
+}
+function myfunoff() {
+  a.style.animationDuration = 0 + "s";
+}
+function myfun1() {
+  a.style.animationDuration = 1 + "s";
+}
+function myfun2() {
+  a.style.animationDuration = 0.5 + "s";
+}
+function myfun3() {
+  a.style.animationDuration = 0.2 + "s";
 }
